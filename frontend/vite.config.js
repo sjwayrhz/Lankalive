e6bddcc93bf90bc2b927e7ffd814f0b24f9590e4
sjwayrhz <sjwayrhz@gmail.com>
2025-11-23 16:37:24 +0800

    front port 5173 to 3000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0', // Allow external connections
-    port: 5173, // Default Vite port
+    port: 3000, // Default Vite port
     proxy: {
       '/api': {
         target: 'http://localhost:8000', // Your Flask backend address
